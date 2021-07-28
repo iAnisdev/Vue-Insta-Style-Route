e34537c650bb23f36f946a930f3c1cee2ac329af
marahmanjs <imarahmandev@gmail.com>
2021-07-28 12:06:28 +0500

    backword handler

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,20 +16,45 @@ export default {
   data(){
     return {
       currencies: ['USD', 'AED' , 'GBP' , 'PKR'],
-      currency: ''
+      currency: '',
+      questionIndex: 0
     }
   },
   methods:{
     viewCurrency(currency){
       this.currency = currency
       this.$modal.show('currency-modal')
-      window.history.pushState("", "", currency);
+      window.history.pushState({}, "", currency);
     },
     modelCloseHandler(){
-      window.history.pushState("", "", '/');
-      this.currency = {}
+      window.history.pushState({}, "", '/');
+      this.currency = ''
+    },
+    PopStateEventHandler(event){
+    let that = this
+      if(event.currentTarget.location.pathname === '/'){
+          that.currency = ''
+          that.$modal.hide('currency-modal')
+       }else{
+        //  let targetCurrency = event.currentTarget.location.pathname.slice(1)
+        //  console.log('targetCurrency' , targetCurrency ,event.currentTarget.location)
+        //  if(that.currencies.indexOf(targetCurrency) !== -1){
+        //    event.preventDefault()
+        //     that.currency = that.currencies[that.currencies.indexOf(targetCurrency)]
+        //     that.$modal.show('currency-modal')
+        //     window.history.pushState({}, "", that.currency);
+        //  }else{
+        //    //let the route behave as normal
+        //  }
+       }
     }
-  }
+  },
+  mounted() {
+     window.addEventListener('popstate', this.PopStateEventHandler);
+ },
+ destroyed(){
+    window.removeEventListener('popstate' , this.PopStateEventHandler)
+ }
 }
 </script>
 
